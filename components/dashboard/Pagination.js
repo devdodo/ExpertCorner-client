2b10b97f177488ee284560d9fba0409af98b544b
pauljsd <paul.atolagbe.ayobami@gmail.com>
2023-12-07 11:27:01 +0100

    changes made by Frank

--- components/dashboard/Pagination.js
+++ components/dashboard/Pagination.js
@@ -1,35 +1,64 @@
-import React from 'react'
+import React from "react";
 
-const Pagination = ({postsPerPage, totalPosts, paginate, currentPage}) => {
-    const pageNumbers = []
+const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage }) => {
+  const pageNumbers = [];
 
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  console.log(pageNumbers);
 
-    for(let i = 1; i <= Math.ceil(totalPosts/postsPerPage); i++){
-        pageNumbers.push(i)
-    }
-    console.log(pageNumbers);
-
-    return(
-        <div>
-            <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-                <a href="#" class="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-                <span class="sr-only">Previous</span>
-                <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fill-rule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clip-rule="evenodd" />
-                </svg>
-                </a>
-                {pageNumbers.map(number => (
-                    <div onClick={() => paginate(number)} key={number} aria-current="page" class={`relative z-10 inline-flex items-center ${currentPage == number?"bg-indigo-600 text-white": "text-black"} px-4 py-2 text-sm font-semibold  focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}>{number}</div>
-                ))}
-                <a href="#" class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-                <span class="sr-only">Next</span>
-                <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
-                </svg>
-                </a>
-            </nav>
+  return (
+    <div>
+      <nav
+        class="isolate inline-flex -space-x-px rounded-md shadow-sm"
+        aria-label="Pagination"
+      >
+        <div class="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+          <span class="sr-only">Previous</span>
+          <svg
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
+              clip-rule="evenodd"
+            />
+          </svg>
         </div>
-    )
-}
+        {pageNumbers.map((number) => (
+          <div
+            onClick={() => paginate(number)}
+            key={number}
+            aria-current="page"
+            class={`relative z-10 inline-flex items-center ${
+              currentPage == number ? "bg-indigo-600 text-white" : "text-black"
+            } px-4 py-2 text-sm font-semibold  focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
+          >
+            {number}
+          </div>
+        ))}
+        <div class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+          <span class="sr-only">Next</span>
+          <svg
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default Pagination;
--- components/dashboard/aside.js
+++ components/dashboard/aside.js
@@ -1,82 +1,94 @@
 import Link from "next/link";
-import { FaCalendar, FaHome, FaUser, FaCog, FaSignOutAlt } from "react-icons/fa";
-import { useEffect } from 'react';
+import {
+  FaCalendar,
+  FaHome,
+  FaUser,
+  FaCog,
+  FaSignOutAlt,
+} from "react-icons/fa";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { logout } from "../../utilities/slices/userSlice";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
+const Aside = ({ index }) => {
+  const dispatch = useDispatch();
+  const router = useRouter();
 
-const Aside = ({index}) => {
-    const dispatch = useDispatch();
-    const router = useRouter();
+  const handleLogout = () => {
+    dispatch(logout());
+    router.push("/signin", "/signin");
+    return;
+  };
 
-    const handleLogout = () => {
-        dispatch(logout());
-        router.push("/signin", "/signin");
-        return;
-    }
+  const nav = [
+    {
+      title: "Dashboard",
+      link: "/dashboard",
+      icon: <FaHome className="text-xl mr-3" />,
+      active: false,
+    },
+    {
+      title: "Request",
+      link: "/request",
+      icon: <FaCalendar className="text-xl mr-3" />,
+      active: false,
+    },
+    {
+      title: "Profile",
+      link: "/profile",
+      icon: <FaUser className="text-xl mr-3" />,
+      active: false,
+    },
+    // {
+    //     title: "Settings",
+    //     link: "/settings",
+    //     icon: <FaCog className="text-xl mr-3" />,
+    //     active: false
+    // }
+  ];
 
-    const nav = [
-        {
-            title: "Dashboard",
-            link: "/dashboard",
-            icon: <FaHome className="text-xl mr-3" />,
-            active: false
-        },
-        {
-            title: "Request",
-            link: "/request",
-            icon: <FaCalendar className="text-xl mr-3" />,
-            active: false
-        },
-        {
-            title: "Profile",
-            link: "/profile",
-            icon: <FaUser className="text-xl mr-3" />,
-            active: false
-        },
-        // {
-        //     title: "Settings",
-        //     link: "/settings",
-        //     icon: <FaCog className="text-xl mr-3" />,
-        //     active: false
-        // }
-    ]
+  nav[index].active = true;
 
-    nav[index].active = true
-
-
-    return (
-        <div className="bg-white h-screen w-72 hidden fixed md:block border-r border-r-slate-200">
-            <div className="p-8 mb-4">
-                <Link href="index.html" className="w-full text-3xl flex justify-center">
-                    <img src="../../img/new-logo.png" className="w-36" />
-                </Link>
-                <div className="text-center">
-                    <h3 className="mt-4 text-sm font-bold text-gray-300">Control Center</h3>
-                </div>
-            </div> 
-            <div className="flex flex-col text-gray-800 text-base font-semibold pt-3">
-                {nav.map((row, index) => (
-                    row.active 
-                    ?
-                    <Link key={index} href={row.link} className="flex items-center text-md bg-gray-100 text-indigo-700 py-4 pl-6 div-item mb-2">
-                        {row.icon}
-                        {row.title}
-                    </Link>
-                    : 
-                    <Link key={index} href={row.link} className="flex items-center text-md  text-gray-500 hover:bg-gray-100 hover:text-indigo-500 py-4 pl-6 div-item mb-2">
-                        {row.icon}
-                        {row.title}
-                    </Link>
-                ))}
-                <div onClick={handleLogout} className="flex items-center text-md cursor-pointer text-gray-500 hover:bg-gray-100 hover:text-indigo-500 py-4 pl-6 div-item mb-2">
-                    <FaSignOutAlt className="text-xl mr-3" />
-                    Logout
-                </div>
-            </div>
+  return (
+    <div className="bg-white h-screen w-72 hidden fixed md:block border-r border-r-slate-200">
+      <div className="p-8 mb-4">
+        <Link href="/dashboard" className="w-full text-3xl flex justify-center">
+          <img src="../../img/new-logo.png" className="w-36" />
+        </Link>
+      </div>
+      <div className="flex flex-col text-gray-800 text-base font-semibold pt-3">
+        {nav.map((row, index) =>
+          row.active ? (
+            <Link
+              key={index}
+              href={row.link}
+              className="flex items-center text-md bg-gray-100 text-indigo-700 py-4 pl-6 div-item mb-2"
+            >
+              {row.icon}
+              {row.title}
+            </Link>
+          ) : (
+            <Link
+              key={index}
+              href={row.link}
+              className="flex items-center text-md  text-gray-500 hover:bg-gray-100 hover:text-indigo-500 py-4 pl-6 div-item mb-2"
+            >
+              {row.icon}
+              {row.title}
+            </Link>
+          )
+        )}
+        <div
+          onClick={handleLogout}
+          className="flex items-center text-md cursor-pointer text-gray-500 hover:bg-gray-100 hover:text-indigo-500 py-4 pl-6 div-item mb-2"
+        >
+          <FaSignOutAlt className="text-xl mr-3" />
+          Logout
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Aside;
