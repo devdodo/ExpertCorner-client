777bd0aac1ef3b141cd99aaa8c7cc9034772f780
Frank Ameh <ameh.frank@yahoo.com>
2023-12-09 15:46:29 +0100

    Dark mode

--- components/signUp.js
+++ components/signUp.js
@@ -8,7 +8,7 @@ const SignUp = () => {
                 <div className="flex justify-center">
                     <div className="flex min-h-full w-full md:w-1/3 flex-col justify-center px-6 py-12 lg:px-8 border rounded">
                     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign Up</h2>
+                        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-white">Sign Up</h2>
                     </div>
 
                     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
